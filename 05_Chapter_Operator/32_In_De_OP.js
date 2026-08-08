be9d9efd162ce491228780e132c8822ae3ee36da
pankajydv-web <pkyydv5@gmail.com>
2026-08-08 14:40:22 +0530

    added javascript content in feature/premian

---
+++ 05_Chapter_Operator/32_In_De_OP.js
@@ -0,0 +1,14 @@
+// Pre Increment
+// let a = 10;
+// let b = ++a;
+// console.log(b);
+
+
+// Post Increment
+let a = 10;
+let b = a++;
+let c = ++b;
+let d = ++c;
+console.log(b);
+console.log(c);
+console.log(d);
