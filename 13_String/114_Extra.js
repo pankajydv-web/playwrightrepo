0211aaddc2d325f5f6cd3838f1c23f410b8ae832
pankajydv-web <pkyydv5@gmail.com>
2026-08-14 00:08:21 +0530

    update remaining codes

---
+++ 13_String/114_Extra.js
@@ -0,0 +1,13 @@
+let url = "https://app.vwo.com?app=pramod";
+console.log(url.replace(/app/g, "qa"));
+
+console.log(url);  // only first occurrence
+
+// Splitting & Joining
+"pass,fail,skip".split(",");  // ["pass", "fail", "skip"]
+"hello".split("");  // ["h","e","l","l","o"]
+
+// Template literal (joining with format)
+let parts = ["2024", "03", "07"];
+let date = parts.join("-");
+console.log(date);
